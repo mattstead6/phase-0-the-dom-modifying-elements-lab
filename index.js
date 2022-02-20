e6541b44a45ce809c8d5a5def2db3198101a581d
Matthew Stead <mattstead6@gmail.com>
2022-02-20 16:46:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const ourBody = document.body;
+const mainElement = document.getElementById("main");
+ourBody.removeChild(mainElement);
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "is the champion";
+
