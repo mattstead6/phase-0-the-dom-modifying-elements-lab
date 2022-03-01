c8edaf71880795833aa04493dc8c68aeb9aace3f
Matthew Stead <mattstead6@gmail.com>
2022-02-28 22:42:04 -0500

    commit tree fe30e64d5796b7f8143f31e97ff86027bd691ea3

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 // Write your code here!
 
-const ourBody = document.body;
-const mainElement = document.getElementById("main");
-ourBody.removeChild(mainElement);
+const main = document.querySelector('#main')
+main.remove()
 
-const newHeader = document.createElement("h1");
 
-newHeader.setAttribute("id", "victory");
+const newHeader = document.createElement('h1') 
+newHeader.id = 'victory'
+newHeader.textContent = "is the champion"
 
-newHeader.textContent = "is the champion";
+
+
+
+
+
+
+
+// const ourBody = document.body;
+// const mainElement = document.getElementById("main");
+// ourBody.removeChild(mainElement);
+
+// const newHeader = document.createElement("h1");
+
+// newHeader.setAttribute("id", "victory");
+
+// newHeader.textContent = "is the champion";
 
